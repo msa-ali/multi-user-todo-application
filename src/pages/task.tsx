2e6450f6bef5fb6e06e3e9e8695bc581c48a6b56
Altamash Ali <14983914+Altamashattari@users.noreply.github.com>
2023-04-15 14:09:12 +0530

    delete task

--- src/pages/task.tsx
+++ src/pages/task.tsx
@@ -1,10 +1,17 @@
 import AddTask from "../components/add-task";
 import TaskCard from "../components/task-card";
-import { useAppSelector } from "../store";
+import { deleteTask } from "../features/task";
+import { useAppDispatch, useAppSelector } from "../store";
 import { MdDeleteForever } from 'react-icons/md';
 
 function TaskPage() {
   const tasks = useAppSelector(state => state.tasks.entities);
+
+  const dispatch = useAppDispatch();
+
+  const onTaskDelete = (id: string) => {
+    dispatch(deleteTask(id));
+  }
 
   return (
     <div className="flex flex-col justify-center items-center h-full ">
@@ -14,7 +21,10 @@ function TaskPage() {
         return (
           <div key={task.id} className="flex items-center gap-4">
             <TaskCard key={task.id} task={task} />
-            <MdDeleteForever className="text-3xl text-gray-600 cursor-pointer hover:text-red-600" />
+            <MdDeleteForever 
+              className="text-3xl text-gray-600 cursor-pointer hover:text-red-600"
+              onClick={() => onTaskDelete(task.id)}
+            />
           </div>
           
         );
